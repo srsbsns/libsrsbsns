bec82a4fa63233d97cebe22ab2edc4fb2e59c57c
fisted <van.fisted@googlemail.com>
2014-02-23 02:28:11 +0100

    test_deque: pushpop unit test

--- unittests/test_deque.c
+++ unittests/test_deque.c
@@ -6,6 +6,8 @@
 
 #include <libsrsbsns/deque.h>
 
+#define COUNTOF(ARR) (sizeof (ARR) / sizeof (ARR)[0])
+
 const char* /*UNITTEST*/
 test_basic(void)
 {
@@ -21,6 +23,83 @@ test_basic(void)
 const char* /*UNITTEST*/
 test_pushpop(void)
 {
+	deque_t d = deque_init(10);
+	int ia[] = {0,1,2,3,4,5,6,7,8,9};
+	for (size_t i = 0; i < COUNTOF(ia); i++)
+		if (!deque_pushback(d, &i))
+			return "pushback failed";
+	
+	if (deque_count(d) != COUNTOF(ia))
+		return "wrong count after pushback";
+
+	int *e;
+	for (size_t i = 0; i < COUNTOF(ia); i++) {
+		if (!(e = deque_popback(d)))
+			return "popback failed";
+
+		if (*e != ia[9-i])
+			return "wrong element popped out the back";
+	}
+
+	if (deque_count(d) != 0)
+		return "deque supposed to be empty but it's not";
+
+	for (size_t i = 0; i < COUNTOF(ia); i++)
+		if (!deque_pushback(d, &i))
+			return "pushback failed (2)";
+	
+	if (deque_count(d) != COUNTOF(ia))
+		return "wrong count after pushback (2)";
+
+	for (size_t i = 0; i < COUNTOF(ia); i++) {
+		if (!(e = deque_popfront(d)))
+			return "popfront failed";
+
+		if (*e != ia[i])
+			return "wrong element popped out the front";
+	}
+
+	if (deque_count(d) != 0)
+		return "deque supposed to be empty but it's not (2)";
+
+	for (size_t i = 0; i < COUNTOF(ia); i++)
+		if (!deque_pushfront(d, &i))
+			return "pushfront failed";
+	
+	if (deque_count(d) != COUNTOF(ia))
+		return "wrong count after pushfront";
+
+	for (size_t i = 0; i < COUNTOF(ia); i++) {
+		if (!(e = deque_popback(d)))
+			return "popback failed (2)";
+
+		if (*e != ia[i])
+			return "wrong element popped out the back (2)";
+	}
+
+	if (deque_count(d) != 0)
+		return "deque supposed to be empty but it's not (3)";
+
+	for (size_t i = 0; i < COUNTOF(ia); i++)
+		if (!deque_pushfront(d, &i))
+			return "pushfront failed (2)";
+	
+	if (deque_count(d) != COUNTOF(ia))
+		return "wrong count after pushfront (2)";
+
+	for (size_t i = 0; i < COUNTOF(ia); i++) {
+		if (!(e = deque_popfront(d)))
+			return "popfront failed (2)";
+
+		if (*e != ia[9-i])
+			return "wrong element popped out the front (2)";
+	}
+
+	if (deque_count(d) != 0)
+		return "deque supposed to be empty but it's not (4)";
+
+	deque_dispose(d);
+
 	return "unit test not implemented";
 }
 
