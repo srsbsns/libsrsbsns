650ca68fb2a2028c69e950177f171e3c9f3a9dd9
fisted <van.fisted@googlemail.com>
2014-02-23 20:22:26 +0100

    test_deque: remove over9000 deque_dump() calls

--- unittests/test_deque.c
+++ unittests/test_deque.c
@@ -29,12 +29,10 @@ const char* /*UNITTEST*/
 test_pushpop(void)
 {
 	deque_t d = deque_init(20);
-	deque_dump(d, idump);
 	int ia[] = {0,1,2,3,4,5,6,7,8,9};
 	for (size_t i = 0; i < COUNTOF(ia); i++)
 		if (!deque_pushback(d, &ia[i]))
 			return "pushback failed";
-	deque_dump(d, idump);
 	
 	if (deque_count(d) != COUNTOF(ia))
 		return "wrong count after pushback";
@@ -47,24 +45,20 @@ test_pushpop(void)
 		if (*e != ia[9-i])
 			return "wrong element popped out the back";
 	}
-	deque_dump(d, idump);
 
 	if (deque_count(d) != 0)
 		return "deque supposed to be empty but it's not";
 
 	if (deque_popback(d))
 		return "popback didn't fail when it should (1)";
-	deque_dump(d, idump);
 	if (deque_popfront(d))
 		return "popfront didn't fail when it should (1)";
-	deque_dump(d, idump);
 
 	for (size_t i = 0; i < COUNTOF(ia); i++)
 		if (!deque_pushback(d, &ia[i]))
 			return "pushback failed (2)";
 	
 
-	deque_dump(d, idump);
 	if (deque_count(d) != COUNTOF(ia))
 		return "wrong count after pushback (2)";
 
@@ -76,22 +70,18 @@ test_pushpop(void)
 			return "wrong element popped out the front";
 	}
 
-	deque_dump(d, idump);
 	if (deque_count(d) != 0)
 		return "deque supposed to be empty but it's not (2)";
 
 	if (deque_popback(d))
 		return "popback didn't fail when it should (2)";
-	deque_dump(d, idump);
 	if (deque_popfront(d))
 		return "popfront didn't fail when it should (2)";
-	deque_dump(d, idump);
 
 	for (size_t i = 0; i < COUNTOF(ia); i++)
 		if (!deque_pushfront(d, &ia[i]))
 			return "pushfront failed";
 	
-	deque_dump(d, idump);
 	if (deque_count(d) != COUNTOF(ia))
 		return "wrong count after pushfront";
 
@@ -103,32 +93,22 @@ test_pushpop(void)
 			return "wrong element popped out the back (2)";
 	}
 
-	deque_dump(d, idump);
 	if (deque_count(d) != 0)
 		return "deque supposed to be empty but it's not (3)";
 
-	deque_dump(d, idump);
 	if (deque_popback(d))
 		return "popback didn't fail when it should (3)";
-	deque_dump(d, idump);
 
 	if (deque_popfront(d))
 		return "popfront didn't fail when it should (3)";
 
-	fprintf(stderr, "======\n");
-	deque_dump(d, idump);
-	fprintf(stderr, "======\n");
-	for (size_t i = 0; i < COUNTOF(ia); i++) {
+	for (size_t i = 0; i < COUNTOF(ia); i++)
 		if (!deque_pushfront(d, &ia[i]))
 			return "pushfront failed (2)";
-		deque_dump(d, idump);
-	}
 	
-	deque_dump(d, idump);
 	if (deque_count(d) != COUNTOF(ia))
 		return "wrong count after pushfront (2)";
 
-	deque_dump(d, idump);
 	for (size_t i = 0; i < COUNTOF(ia); i++) {
 		if (!(e = deque_popfront(d)))
 			return "popfront failed (2)";
@@ -136,18 +116,15 @@ test_pushpop(void)
 		if (*e != ia[9-i])
 			return "wrong element popped out the front (2)";
 	}
-	deque_dump(d, idump);
 
 	if (deque_count(d) != 0)
 		return "deque supposed to be empty but it's not (4)";
 
 	if (deque_popback(d))
 		return "popback didn't fail when it should (4)";
-	deque_dump(d, idump);
 	if (deque_popfront(d))
 		return "popfront didn't fail when it should (4)";
 
-	deque_dump(d, idump);
 	deque_dispose(d);
 
 	return NULL;
@@ -157,12 +134,10 @@ const char* /*UNITTEST*/
 test_grow(void)
 {
 	deque_t d = deque_init(0);
-	deque_dump(d, idump);
 	int ia[] = {0,1,2,3,4,5,6,7,8,9};
 	for (size_t i = 0; i < COUNTOF(ia); i++)
 		if (!deque_pushback(d, &ia[i]))
 			return "pushback failed";
-	deque_dump(d, idump);
 	
 	if (deque_count(d) != COUNTOF(ia))
 		return "wrong count after pushback";
@@ -175,17 +150,14 @@ test_grow(void)
 		if (*e != ia[9-i])
 			return "wrong element popped out the back";
 	}
-	deque_dump(d, idump);
 
 	if (deque_count(d) != 0)
 		return "deque supposed to be empty but it's not";
 
 	if (deque_popback(d))
 		return "popback didn't fail when it should (1)";
-	deque_dump(d, idump);
 	if (deque_popfront(d))
 		return "popfront didn't fail when it should (1)";
-	deque_dump(d, idump);
 
 	deque_dispose(d);
 	d = deque_init(0);
@@ -193,7 +165,6 @@ test_grow(void)
 		if (!deque_pushback(d, &ia[i]))
 			return "pushback failed (2)";
 	
-	deque_dump(d, idump);
 	if (deque_count(d) != COUNTOF(ia))
 		return "wrong count after pushback (2)";
 
@@ -205,16 +176,13 @@ test_grow(void)
 			return "wrong element popped out the front";
 	}
 
-	deque_dump(d, idump);
 	if (deque_count(d) != 0)
 		return "deque supposed to be empty but it's not (2)";
 
 	if (deque_popback(d))
 		return "popback didn't fail when it should (2)";
-	deque_dump(d, idump);
 	if (deque_popfront(d))
 		return "popfront didn't fail when it should (2)";
-	deque_dump(d, idump);
 
 	deque_dispose(d);
 	d = deque_init(0);
@@ -222,7 +190,6 @@ test_grow(void)
 		if (!deque_pushfront(d, &ia[i]))
 			return "pushfront failed";
 	
-	deque_dump(d, idump);
 	if (deque_count(d) != COUNTOF(ia))
 		return "wrong count after pushfront";
 
@@ -234,14 +201,11 @@ test_grow(void)
 			return "wrong element popped out the back (2)";
 	}
 
-	deque_dump(d, idump);
 	if (deque_count(d) != 0)
 		return "deque supposed to be empty but it's not (3)";
 
-	deque_dump(d, idump);
 	if (deque_popback(d))
 		return "popback didn't fail when it should (3)";
-	deque_dump(d, idump);
 
 	if (deque_popfront(d))
 		return "popfront didn't fail when it should (3)";
@@ -252,11 +216,9 @@ test_grow(void)
 		if (!deque_pushfront(d, &ia[i]))
 			return "pushfront failed (2)";
 	
-	deque_dump(d, idump);
 	if (deque_count(d) != COUNTOF(ia))
 		return "wrong count after pushfront (2)";
 
-	deque_dump(d, idump);
 	for (size_t i = 0; i < COUNTOF(ia); i++) {
 		if (!(e = deque_popfront(d)))
 			return "popfront failed (2)";
@@ -264,18 +226,15 @@ test_grow(void)
 		if (*e != ia[9-i])
 			return "wrong element popped out the front (2)";
 	}
-	deque_dump(d, idump);
 
 	if (deque_count(d) != 0)
 		return "deque supposed to be empty but it's not (4)";
 
 	if (deque_popback(d))
 		return "popback didn't fail when it should (4)";
-	deque_dump(d, idump);
 	if (deque_popfront(d))
 		return "popfront didn't fail when it should (4)";
 
-	deque_dump(d, idump);
 	deque_dispose(d);
 
 	return NULL;
