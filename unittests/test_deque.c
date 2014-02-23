3113e1e9c9d73a6df8b42ab97d58226f0dde665a
fisted <van.fisted@googlemail.com>
2014-02-23 16:31:40 +0100

    test_deque: add unit test for grow

--- unittests/test_deque.c
+++ unittests/test_deque.c
@@ -115,9 +115,14 @@ test_pushpop(void)
 	if (deque_popfront(d))
 		return "popfront didn't fail when it should (3)";
 
-	for (size_t i = 0; i < COUNTOF(ia); i++)
+	fprintf(stderr, "======\n");
+	deque_dump(d, idump);
+	fprintf(stderr, "======\n");
+	for (size_t i = 0; i < COUNTOF(ia); i++) {
 		if (!deque_pushfront(d, &ia[i]))
 			return "pushfront failed (2)";
+		deque_dump(d, idump);
+	}
 	
 	deque_dump(d, idump);
 	if (deque_count(d) != COUNTOF(ia))
@@ -151,7 +156,129 @@ test_pushpop(void)
 const char* /*UNITTEST*/
 test_grow(void)
 {
-	return "unit test not implemented";
+	deque_t d = deque_init(0);
+	deque_dump(d, idump);
+	int ia[] = {0,1,2,3,4,5,6,7,8,9};
+	for (size_t i = 0; i < COUNTOF(ia); i++)
+		if (!deque_pushback(d, &ia[i]))
+			return "pushback failed";
+	deque_dump(d, idump);
+	
+	if (deque_count(d) != COUNTOF(ia))
+		return "wrong count after pushback";
+
+	int *e;
+	for (size_t i = 0; i < COUNTOF(ia); i++) {
+		if (!(e = deque_popback(d)))
+			return "popback failed";
+
+		if (*e != ia[9-i])
+			return "wrong element popped out the back";
+	}
+	deque_dump(d, idump);
+
+	if (deque_count(d) != 0)
+		return "deque supposed to be empty but it's not";
+
+	if (deque_popback(d))
+		return "popback didn't fail when it should (1)";
+	deque_dump(d, idump);
+	if (deque_popfront(d))
+		return "popfront didn't fail when it should (1)";
+	deque_dump(d, idump);
+
+	deque_dispose(d);
+	d = deque_init(0);
+	for (size_t i = 0; i < COUNTOF(ia); i++)
+		if (!deque_pushback(d, &ia[i]))
+			return "pushback failed (2)";
+	
+	deque_dump(d, idump);
+	if (deque_count(d) != COUNTOF(ia))
+		return "wrong count after pushback (2)";
+
+	for (size_t i = 0; i < COUNTOF(ia); i++) {
+		if (!(e = deque_popfront(d)))
+			return "popfront failed";
+
+		if (*e != ia[i])
+			return "wrong element popped out the front";
+	}
+
+	deque_dump(d, idump);
+	if (deque_count(d) != 0)
+		return "deque supposed to be empty but it's not (2)";
+
+	if (deque_popback(d))
+		return "popback didn't fail when it should (2)";
+	deque_dump(d, idump);
+	if (deque_popfront(d))
+		return "popfront didn't fail when it should (2)";
+	deque_dump(d, idump);
+
+	deque_dispose(d);
+	d = deque_init(0);
+	for (size_t i = 0; i < COUNTOF(ia); i++)
+		if (!deque_pushfront(d, &ia[i]))
+			return "pushfront failed";
+	
+	deque_dump(d, idump);
+	if (deque_count(d) != COUNTOF(ia))
+		return "wrong count after pushfront";
+
+	for (size_t i = 0; i < COUNTOF(ia); i++) {
+		if (!(e = deque_popback(d)))
+			return "popback failed (2)";
+
+		if (*e != ia[i])
+			return "wrong element popped out the back (2)";
+	}
+
+	deque_dump(d, idump);
+	if (deque_count(d) != 0)
+		return "deque supposed to be empty but it's not (3)";
+
+	deque_dump(d, idump);
+	if (deque_popback(d))
+		return "popback didn't fail when it should (3)";
+	deque_dump(d, idump);
+
+	if (deque_popfront(d))
+		return "popfront didn't fail when it should (3)";
+
+	deque_dispose(d);
+	d = deque_init(0);
+	for (size_t i = 0; i < COUNTOF(ia); i++)
+		if (!deque_pushfront(d, &ia[i]))
+			return "pushfront failed (2)";
+	
+	deque_dump(d, idump);
+	if (deque_count(d) != COUNTOF(ia))
+		return "wrong count after pushfront (2)";
+
+	deque_dump(d, idump);
+	for (size_t i = 0; i < COUNTOF(ia); i++) {
+		if (!(e = deque_popfront(d)))
+			return "popfront failed (2)";
+
+		if (*e != ia[9-i])
+			return "wrong element popped out the front (2)";
+	}
+	deque_dump(d, idump);
+
+	if (deque_count(d) != 0)
+		return "deque supposed to be empty but it's not (4)";
+
+	if (deque_popback(d))
+		return "popback didn't fail when it should (4)";
+	deque_dump(d, idump);
+	if (deque_popfront(d))
+		return "popfront didn't fail when it should (4)";
+
+	deque_dump(d, idump);
+	deque_dispose(d);
+
+	return NULL;
 }
 
 const char* /*UNITTEST*/
