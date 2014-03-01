cfcf689ad885936893d7bbc749b3750ce115fab3
fisted <van.fisted@googlemail.com>
2014-03-01 02:50:48 +0100

    graph: actually i have no idea where this leads...

--- include/libsrsbsns/graph.h
+++ include/libsrsbsns/graph.h
@@ -8,4 +8,11 @@
 typedef struct node *node_t;
 typedef struct edge *edge_t;
 
+node_t alloc_node(void* elem);
+void* get_elem(node_t n);
+
+size_t num_chld(node_t n);
+size_t num_nattr(node_t n);
+edge_t link_nodes(node_t from, node_t to, double w);
+
 #endif /* LIBSRSBSNS_GRAPH_H */
--- libsrsbsns/graph.c
+++ libsrsbsns/graph.c
@@ -20,3 +20,49 @@ struct node {
 	void *elem;
 	deque_t attr;
 };
+
+
+node_t
+alloc_node(void* elem)
+{
+	struct node *n = malloc(sizeof *n);
+
+	n->attr = NULL;
+	n->ch = NULL;
+	n->elem = elem;
+
+	return n;
+}
+
+void*
+get_elem(node_t n)
+{
+	return n->elem;
+}
+
+size_t
+num_chld(node_t n)
+{
+	return deque_count(n->ch);
+}
+
+size_t
+num_nattr(node_t n)
+{
+	return deque_count(n->attr);
+}
+
+edge_t
+link_nodes(node_t from, node_t to, double w)
+{
+	edge_t e = malloc(sizeof *e);
+
+	e->attr = NULL;
+	e->from = from;
+	e->to = to;
+	e->weight = w;
+
+	deque_pushback(from->ch, e);
+
+	return e;
+}
