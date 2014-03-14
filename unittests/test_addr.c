94f84d8d39dae7c44faced528ffccb73695bfcf3
fisted <van.fisted@googlemail.com>
2014-03-14 21:46:39 +0100

    test_addr: start unit test

---
+++ unittests/test_addr.c
@@ -0,0 +1,15 @@
+/* test_addr.c - (C) 2014, Timo Buhrmester
+ * libsrsbsns - A srs lib
+  * See README for contact-, COPYING for license information.  */
+
+#include "unittests_common.h"
+
+#include <libsrsbsns/addr.h>
+
+#define COUNTOF(ARR) (sizeof (ARR) / sizeof (ARR)[0])
+
+const char* /*UNITTEST*/
+test_basic(void)
+{
+	return NULL;
+}
