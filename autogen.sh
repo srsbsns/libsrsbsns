e1d343b8870e756a417c504bb44ac175daf25311
eeeee <eeeee@users.noreply.github.com>
2016-02-21 12:44:44 -0800

    De-facto industry standard money nag notice
    Academic tradition requires you to cite works you base your article on (or to just pay cash under the table).

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+>&2 echo "When using programs that use libsrsbsns please cite:"
+>&2 echo "van fisted (2011): libsrsbsns A serious library"
+>&2 echo ";github: Award winning unique innovative solution for cutting edge revolutionary business application, February 2011:13-37."
+>&2 echo "Or you can get libsrsbsns without this requirement by paying over 9000 EUR."
+
 mkdir m4 build-aux &> /dev/null
 autoreconf -i || exit 1
